b3aeef53ccb766160f3758a1e8c86f6700f1c090
Tim Lum <twhlum@gmail.com>
2018-09-20 10:39:43 -0700

    Update 0905_SortArrayByParity.cpp
    Removed use of auxiliary data structures. Utilized in place swapping.

--- Algorithms/0905_SortArrayByParity.cpp
+++ Algorithms/0905_SortArrayByParity.cpp
@@ -3,26 +3,13 @@
 class Solution {
 public:
   vector<int> sortArrayByParity(vector<int>& A) {
-    queue<int> odds;
-    queue<int> evens;
-    for( int i = 0 ; i < A.size() ; i++ ) {
-      if( A[i] % 2 == 0 ) {
-        evens.push( A[i] );
-      }
-      else {
-        odds.push( A[i] );
+    int currIndex = 0;
+    for( int i = 0 ; i < A.size() ; i++ ){
+      if( A[i] % 2 == 0 ){
+        swap( A[i], A[currIndex] );
+        currIndex++;
       }
     }
-      
-    vector<int> retVector;
-    while( !evens.empty( ) ) {
-      retVector.push_back( evens.front( ) );
-      evens.pop( );
-    }
-    while( !odds.empty( ) ) {
-      retVector.push_back( odds.front( ) );
-      odds.pop( );
-    }
-    return( retVector );
+    return( A );
   }
 };
